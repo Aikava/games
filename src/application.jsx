5971a2c82ac71acdada65ae6a8bc11f35b980fb7
Ry Aikava <aikava.kizuna@gmail.com>
2024-10-26 21:46:37 +0300

    added card

--- src/application.jsx
+++ src/application.jsx
@@ -1,16 +1,44 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import "./style.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const mountPoint = document.querySelector(".application");
 const root = createRoot(mountPoint);
-
+const userDeck = [
+    {
+        hp: 10,
+        atk: 1,
+        def: 1,
+        description: "Basic card"
+    }
+];
+const Card = ({ hp, atk, def, description }) => {
+    return (
+        <div className="card">
+            <div className="card-stats">
+                <b>HP: {hp}</b>
+                <b>ATK: {atk}</b>
+                <b>DEF: {def}</b>
+            </div>
+            <div className="card-image"></div>
+            <p className="card-description">
+                {description}
+            </p>
+        </div>
+    );
+};
 const Home = () => {
-  return (<h1>Home</h1>);
+    return (<>
+            <h1>Home</h1>
+            {userDeck.map((item) => {
+                return <Card {...item} />
+            })}
+        </>
+    );
 };
 const Island = () => {
-  return (<h1>Island</h1>);
+    return (<h1>Island</h1>);
 };
 const Village = () => {
     return (<h1>Village</h1>);
@@ -20,14 +48,14 @@ const UnknownTerritory = () => {
 };
 
 const ApplicationHeader = () => {
-  return (<header className="app-header">
-          <nav className="app-nav">
-          <a href="/">Home</a>
-          <a href="/island">Island</a>
-          <a href="/village">Village</a>
-          <a href="/unknown-territory">Unknown territory</a>
-          </nav>
-          </header>);
+    return (<header className="app-header">
+        <nav className="app-nav">
+            <a href="/">Home</a>
+            <a href="/island">Island</a>
+            <a href="/village">Village</a>
+            <a href="/unknown-territory">Unknown territory</a>
+        </nav>
+    </header>);
 };
 
 const ApplicationBody = () => {
@@ -44,12 +72,12 @@ const ApplicationBody = () => {
 };
 
 export const Application = () => {
-  return (
-    <>
-      <ApplicationHeader />
-      <ApplicationBody />
-   </>
-  );
+    return (
+        <>
+            <ApplicationHeader/>
+            <ApplicationBody/>
+        </>
+    );
 };
 
-root.render(<Application />);
+root.render(<Application/>);
